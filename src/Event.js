c943c48a8841ec2e00c08a7c83e04151d24ad5bc
Adam Malej <adammalej1@gmail.com>
2020-12-11 22:28:57 +0100

    Display willing people

--- src/Event.js
+++ src/Event.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { months } from './constants/date';
 import './Event.scss';
 
 export const Event = ({ event }) => {
-  const { register, handleSubmit, reset } = useForm();
-
   return (
     <div
       className="col-12 col-md-5 event"
@@ -14,14 +13,26 @@ export const Event = ({ event }) => {
       }}
     >
       <div className="row">
-        <div className="col-6">
-          <label>Data wydarzenia: {event.date}</label>
+        <div className="col-9">
+          <label>
+            Data wydarzenia: <b>{convertDate(event.date)}</b>
+          </label>
         </div>
-        <div className="col-6 cost">
+        <div className="col-3 cost">
           <label>Koszt: {event.costPerPerson}</label>
         </div>
         <div className="col-6">
-          <label>Gdzie gramy?: {event.address}</label>
+          <label>Gdzie gramy?:</label>
+        </div>
+        <div className="col-12">{event.address}</div>
+        <div>
+          <ol>
+            {event.willingPersons
+              ? event.willingPersons.map((willingPerson) => {
+                  return <li>{willingPerson}</li>;
+                })
+              : null}
+          </ol>
         </div>
       </div>
       <form>
@@ -38,4 +49,15 @@ export const Event = ({ event }) => {
 const onsubmit = () => {
   alert('Cześć! Przycisk jest w trakcie prac ;)');
   return false;
+};
+
+const convertDate = (date) => {
+  const splitedDate = date.split('-').reverse();
+  const mappedDate = splitedDate.map((date, index) => {
+    if (index === 1) {
+      return months[parseInt(date) - 1].toLowerCase();
+    }
+    return date;
+  });
+  return mappedDate.join(' ');
 };
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -9,6 +9,7 @@ export const getEventsQueries = gql`
       time
       numberOfPerson
       costPerPerson
+      willingPersons
     }
   }
 `;
