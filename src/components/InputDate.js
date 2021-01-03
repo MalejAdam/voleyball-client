d40a4e9a0fb85dd49c166a05a156085b0a695915
Adam Malej <adammalej1@gmail.com>
2021-01-03 16:26:27 +0100

    Possible to set current or future date

--- src/components/InputDate.js
+++ src/components/InputDate.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './InputDate.scss';
 
 export const InputDate = React.forwardRef(({ inputName }, ref) => {
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+  const dateInString =
+    year + '-' + (month < 10 ? '0' + month : month) + '-' + (day < 10 ? '0' + day : day);
+
   return (
     <>
       <div className="col-4 label-col">
         <label>Data: </label>
       </div>
       <div className="col-8">
-        <input name={inputName} ref={ref} type="date" />
+        <input name={inputName} ref={ref} type="date" min={dateInString} />
       </div>
     </>
   );
